0706567a077aa6973fd1bacdfc0ad0214ab47552
Ian Rose <ianrosedev@gmail.com>
2018-02-05 15:18:24 -0500

    Added nested attributes to portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,6 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
-  accepts_nested_attributes_for :technologies, reject_if: -> (attrs) { attrs['name'].blank? }
+  accepts_nested_attributes_for :technologies, reject_if: -> (attr) { attr['name'].blank? }
 
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
